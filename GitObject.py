470374bfa182ef271ff9da0e0c38fd64bcc5f865
Jacob Lambert <jacob.lambert@gmail.com>
2020-01-17 14:43:13 -0500

    Add blob objects

--- GitObject.py
+++ GitObject.py
@@ -43,3 +43,14 @@ class GitObject:
                 f.write(zlib.compress(result))
 
         return sha
+
+
+class GitBlob(GitObject):
+
+    fmt = b"blob"
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data
